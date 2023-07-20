1a7ac711bd1a63544a8ef9870dfc7a5c62e1f59c
liad dagan <127447275+Liad1998@users.noreply.github.com>
2023-07-20 19:46:25 +0300

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,9 +1,9 @@
-def factorial(n):
+def Factorial(n):
   ans = 1
   for i in range(2,n+1):
     ans*=i
   return ans
-def prime(n):
+def Prime(n):
   for i in range(2,n):
     if n%i == 0:
       return False
